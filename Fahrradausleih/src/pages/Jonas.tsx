387612d0316e205c17be0b06259d99b82e97c3fd
LuJo0205 <jonas.haberditzl@gmail.com>
2023-02-23 15:12:45 +0100

    Jonas

---
+++ Fahrradausleih/src/pages/Jonas.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+
+
+
+function jonas() {
+
+  const navigate = useNavigate();
+  return (
+    <body>
+    
+<div className="header">
+  <h1>Hallooooooooooo </h1>
+  <p> Wir sind für sie dar</p>
+</div>
+
+<div className="navbar">
+        <a onClick={() => {
+                            navigate('/', {replace: false});
+                        }}>Home</a>
+        <a onClick={() => {
+                            navigate('/Jonas', {replace: false});
+                        }}>Jonas</a>
+        <a href="#">Link</a>
+        <a href="#" className="right">Link</a>
+      </div>
+
+</body>
+  )
+}
+
+export default jonas
